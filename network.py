e92b0fb2719fa701b4f2ceafeb6f3962cd8777e2
LikeJust <diddydwns135@gmail.com>
2018-11-10 22:27:24 +0900

    my tf keras network

--- network.py
+++ network.py
@@ -14,53 +14,38 @@
 # limitations under the License.
 
 import tensorflow as tf
+from tensorflow import keras
 
-import keras
-from keras.models import Model
-from keras.layers.merge import dot
-from keras.layers import Dense, Input
-from keras.layers.core import Reshape
-
-from keras.layers.embeddings import Embedding
-from keras.layers.core import Dropout, Activation
-
-from misc import get_logger, Option
-opt = Option('./config.json')
+from misc import get_logger, get_json
+opt = get_json('./config.json')
 
 
 def top1_acc(x, y):
-    return keras.metrics.top_k_categorical_accuracy(x, y, k=1)
+    return tf.keras.metrics.top_k_categorical_accuracy(x, y, k=1)
 
 
-class TextOnly:
-    def __init__(self):
-        self.logger = get_logger('textonly')
+class TextModel:
+    def __init__(self, num_classes):
+        logger = get_logger('textonly')
+        max_len = opt['max_len']
+        voca_size = opt['unigram_hash_size'] + 1
 
-    def get_model(self, num_classes, activation='sigmoid'):
-        max_len = opt.max_len
-        voca_size = opt.unigram_hash_size + 1
+        t_input = tf.keras.Input(shape=(max_len,))
+        tx = keras.layers.Embedding(voca_size, opt['embd_size'])(t_input)
 
-        with tf.device('/gpu:0'):
-            embd = Embedding(voca_size,
-                             opt.embd_size,
-                             name='uni_embd')
+        w_input = tf.keras.Input(shape=(max_len,))
+        wx = tf.keras.layers.Reshape((max_len, 1))(w_input)
 
-            t_uni = Input((max_len,), name="input_1")
-            t_uni_embd = embd(t_uni)  # token
+        x = tf.keras.layers.dot([tx, wx], axes=1)
+        x = tf.keras.layers.Reshape((opt['embd_size'], ))(x)
+        x = keras.layers.Dense(16, activation=tf.nn.relu)(x)
+        outputs = keras.layers.Dense(num_classes, activation=tf.nn.sigmoid)(x)
 
-            w_uni = Input((max_len,), name="input_2")
-            w_uni_mat = Reshape((max_len, 1))(w_uni)  # weight
+        model = tf.keras.models.Model(inputs=[t_input, w_input], outputs=outputs)
 
-            uni_embd_mat = dot([t_uni_embd, w_uni_mat], axes=1)
-            uni_embd = Reshape((opt.embd_size, ))(uni_embd_mat)
+        model.summary(print_fn=lambda x: logger.info(x))
+        model.compile(loss='binary_crossentropy',
+            optimizer=tf.train.AdamOptimizer(),
+            metrics=[top1_acc])
 
-            embd_out = Dropout(rate=0.5)(uni_embd)
-            relu = Activation('relu', name='relu1')(embd_out)
-            outputs = Dense(num_classes, activation=activation)(relu)
-            model = Model(inputs=[t_uni, w_uni], outputs=outputs)
-            optm = keras.optimizers.Nadam(opt.lr)
-            model.compile(loss='binary_crossentropy',
-                        optimizer=optm,
-                        metrics=[top1_acc])
-            model.summary(print_fn=lambda x: self.logger.info(x))
-        return model
+        self.model = model
